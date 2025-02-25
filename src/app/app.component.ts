5e2aa3ff21375e3880baf9782905870d3a5785cc
shlomog12 <73135976+shlomog12@users.noreply.github.com>
2025-02-25 22:31:34 +0200

    fix foo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ExcelTableComponent } from './excel-table/excel-table.component';
 import { SeatTableComponent } from './seat-table/seat-table.component';
 
 @Component({
